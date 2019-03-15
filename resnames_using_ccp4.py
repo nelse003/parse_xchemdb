407f49cdd846aa3ca1ff4916f1a549223051c40b
Elliot Nelson <nelse003@gmail.com>
2019-03-15 08:28:13 +0000

    Add Assertion error catch to resnames reading, to catch assertion error due to multiple mdoels. This is in a small number of cases as tested, the cause is unknown as pdb does not appear to have multiple models. Add Docstrings to resnames using CCP4

--- resnames_using_ccp4.py
+++ resnames_using_ccp4.py
@@ -32,27 +32,47 @@ def update_progress(progress):
     sys.stdout.flush()
 
 def update_from_pdb(pdb_df):
+    """
+    Carries out cctbx.iotbx depdendent, find resname and B factors
 
+    Parameters
+    ----------
+    pdb_df
+
+    Returns
+    -------
+    pandas.DataFrame:
+
+    """
+    # Load pdb path from DataFrame
+    # need to select first unique value as there will be duplicates
+    # of name for every residue
     pdb = pdb_df.pdb_latest.unique()[0]
 
+    # read into iotbx.hierarchy
     pdb_in = hierarchy.input(file_name=pdb)
+    # read into iotbx.selection cache
     sel_cache = pdb_in.hierarchy.atom_selection_cache()
 
     print(pdb)
 
-    res_df = pdb_df[['resid','chain','alte']]
+    # loop over rows/ residues
     rows = []
     for index, row in pdb_df.iterrows():
 
+        # Get selection object which corresponds to supplied chain residue id and altloc
         sel = sel_cache.selection("chain {} resid {} altloc {}".format(row.chain,
                                                                        row.resid,
                                                                        row.alte))
+        # Select that residue from main hierarchy
         hier = pdb_in.hierarchy.select(sel)
         resnames = []
         for chain in hier.only_model().chains():
             for residue_group in chain.residue_groups():
                 for atom_group in residue_group.atom_groups():
                     resnames.append(atom_group.resname)
+
+                    # Get B factor information on residue by looking a individual atoms
                     b = []
                     for atom in atom_group.atoms():
                         b.append(atom.b)
@@ -60,7 +80,7 @@ def update_from_pdb(pdb_df):
                     mean_b = np.mean(b)
                     std_b  = np.std(b)
 
-
+        # Append information to row
         if len(resnames) == 1:
             row['resname'] = resnames[0]
             row['B_mean'] = mean_b
@@ -71,33 +91,77 @@ def update_from_pdb(pdb_df):
                              "chain {} resid {} altloc {} "
                              "of pdb: {}".format(chain, resid, alte, pdb))
 
-
+    # Append rows
     pdb_df = pd.concat(rows, axis=1)
 
+    # Transpose to get in same orientation as input
     return pdb_df.T
 
 
 def get_resname_for_log_occ(log_occ_csv, log_occ_resname_csv):
 
-    log_df = pd.read_csv(log_occ_csv)
+    """
+    Loop pdb files in csv, get residue names, B factors and append to new columns
 
-    print(len(log_df.pdb_latest.unique()))
+    Parameters
+    ----------
+    log_occ_csv: str
+        path to input csv
+    log_occ_resname_csv: str
+        path to output csv: resname, B_mean, B_std columns added
+
+    Returns
+    -------
+    None
+
+    Notes
+    ------
+    This is trivially parallelisable and takes ~30 minutes for 3000 crystals.
+    TODO Either Parallelise at luigi.Task level or local multiprocessing
+    """
+
+    # Read in CSV
+    log_df = pd.read_csv(log_occ_csv)
 
     log_df_list = []
     for pos, pdb in enumerate(log_df.pdb_latest.unique()):
 
+        # Log occ csv contains a line for each residue involved in complete groups,
+        # Select residue associated with a pdb file
         pdb_df = log_df.loc[log_df['pdb_latest'] == pdb]
-        pdb_df = update_from_pdb(pdb_df)
+
+        # Catching assertion error related to multiple models.
+        # Only appears to effect a few files, so ignoring,as no obvious reasons,
+        # as pdb does nto appear to have multiple models (i.e. NMR like)
+        try:
+            # Get resnames, mean B factor and B factor standard deviation for
+            # each residue involved in complete groups
+            pdb_df = update_from_pdb(pdb_df)
+        except AssertionError:
+            continue
+        # Track progress
         update_progress(float(pos)/float(len(log_df.pdb_latest.unique())))
+        # Append to local storage
         log_df_list.append(pdb_df)
 
     log_occ_resname_df = pd.concat(log_df_list)
-    print(log_occ_resname_df)
-
     log_occ_resname_df.to_csv(log_occ_resname_csv)
 
 def main():
 
+    """
+    Add residue names to occupancy convergence csv
+
+    Returns
+    -------
+    None
+
+    Notes
+    -------
+    Written to be called as a command line script using argparser,
+    so that it can be called using ccp4-python separately
+    from rest of code
+    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("log_occ_csv",
@@ -106,8 +170,9 @@ def main():
     parser.add_argument("log_occ_resname_csv",
                         help="output csv with log info and resnames",
                         type=str)
-
     args = parser.parse_args()
+
+
     get_resname_for_log_occ(args.log_occ_csv, args.log_occ_resname_csv)
 
 if __name__ == "__main__":
